12b2d827ff37fb47cae6af61301aad19fd8c6f73
Saul Shanabrook <s.shanabrook@gmail.com>
2019-05-25 16:55:11 -0400

    Support multiple aggregates and count

--- ibis_vega_transform/transforms/aggregate.py
+++ ibis_vega_transform/transforms/aggregate.py
@@ -3,14 +3,17 @@ import ibis
 
 def aggregate(transform: dict, expr: ibis.Expr) -> ibis.Expr:
     groupby, = transform["groupby"]
-    op, = transform["ops"]
-    field, = transform["fields"]
-    as_, = transform["as"]
+    ops = transform["ops"]
+    fields = transform["fields"]
+    as_ = transform["as"]
     expr = expr.group_by(groupby).aggregate(
-        [getattr(expr[field], _translate_op(op))().name(as_)]
+        [
+            getattr(expr[field] if field else expr, _translate_op(op))().name(as__)
+            for (field, op, as__) in zip(fields, ops, as_)
+        ]
     )
     return expr
 
 
 def _translate_op(op: str) -> str:
-    return {"mean": "mean", "average": "mean"}.get(op, op)
+    return {"mean": "mean", "average": "mean", "count": "count"}.get(op, op)
