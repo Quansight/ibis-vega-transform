ae8e8c39229380bd65406452cc57b0a26e80df84
Saul Shanabrook <s.shanabrook@gmail.com>
2019-07-03 22:41:02 -0400

    support multiple groupby

--- ibis_vega_transform/transforms/aggregate.py
+++ ibis_vega_transform/transforms/aggregate.py
@@ -2,7 +2,7 @@ import ibis
 
 
 def aggregate(transform: dict, expr: ibis.Expr) -> ibis.Expr:
-    groupby, = transform["groupby"]
+    groupby = transform["groupby"]
     # It's undocumented, but an undefined "ops" value defaults to ["count"]'
     # https://github.com/vega/vega/blob/4d10f9da0df0833c90ff259bbd0960f7cb05e3bf/packages/vega-transforms/src/Aggregate.js#L159-L161
     ops = transform.get("ops", ["count"])
