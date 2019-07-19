9f535b8c694575bdb8d2e936b72cfda466df0439
Ian Rose <ian.r.rose@gmail.com>
2019-07-19 09:23:49 -0700

    Cleanup and docstrings.

--- ibis_vega_transform/transforms/aggregate.py
+++ ibis_vega_transform/transforms/aggregate.py
@@ -1,7 +1,25 @@
+from typing import *
+
 import ibis
+import ibis.expr.types as it
 
 
 def aggregate(transform: dict, expr: ibis.Expr) -> ibis.Expr:
+    """
+    Apply a vega aggregate transform to an ibis expression.
+    https://vega.github.io/vega/docs/transforms/aggregate/
+
+    Parameters
+    ----------
+    transform: dict
+        A JSON-able dictionary representing the vega transform.
+    expr: ibis.Expr
+        The expression to which to apply the transform.
+
+    Returns
+    -------
+    transformed_expr: the transformed expression
+    """
     groupby = transform["groupby"]
     # It's undocumented, but an undefined "ops" value defaults to ["count"]'
     # https://github.com/vega/vega/blob/4d10f9da0df0833c90ff259bbd0960f7cb05e3bf/packages/vega-transforms/src/Aggregate.js#L159-L161
@@ -18,7 +36,23 @@ def aggregate(transform: dict, expr: ibis.Expr) -> ibis.Expr:
     return expr
 
 
-def _aggregate(expr, field, op, name):
+def _aggregate(expr: ibis.Expr, field: str, op: str, name: Optional[str]=None) -> ibis.Expr:
+    """
+    Apply an aggregation operation to an expression.
+
+    Parameters
+    ----------
+    expr: ibis.Expr
+        The expr to which to apply the aggregation operation.
+    field: str
+        The name of the field to aggregate.
+    op: str
+        The name of the aggregation operation, given by
+        https://vega.github.io/vega/docs/transforms/aggregate/#ops
+        Not all operations are implemented here.
+    name: Optional[str]
+        A name for the new aggregated value.
+    """
     expr = expr[field] if field else expr
     operation = _translate_op(op)
     if not operation:
@@ -27,7 +61,11 @@ def _aggregate(expr, field, op, name):
     return expr.name(name) if name else expr
 
 
-def _translate_op(op: str) -> str:
+def _translate_op(op: str) -> Optional[Callable[[it.ColumnValue], ibis.Expr]]
+    """
+    Map a vega op (https://vega.github.io/vega/docs/transforms/aggregate/#ops)
+    to an ibis expression operation.
+    """
     return {
         "count": ibis.expr.api.count,
         "distinct": ibis.expr.api.distinct,
