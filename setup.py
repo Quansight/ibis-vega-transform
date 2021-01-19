4fe9eae9d604f0ebc63e18d33d2295ef48e3ea02
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2021-01-19 12:37:47 -0400

    Update setup.py classifiers

--- setup.py
+++ setup.py
@@ -65,7 +65,6 @@ setuptools.setup(
     classifiers=[
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
     ],
