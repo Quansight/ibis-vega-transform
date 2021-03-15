1f3527f7a1266bf2ec7caa0cfe8ba3212c8d0136
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2021-03-15 13:43:34 -0400

    wip

--- setup.py
+++ setup.py
@@ -1,15 +1,24 @@
 """Setup script for Ibis Vega Transform."""
-
-# Standard library imports
 import ast
+import json
 import os
+from pathlib import Path
 
-# Third party imports
 import setuptools
+import jupyter_packaging
+from jupyter_packaging import (
+    create_cmdclass,
+    install_npm,
+    ensure_targets,
+    combine_commands,
+    skip_if_exists,
+)
+from packaging.version import parse
 
-# Constants
-HERE = os.path.abspath(os.path.dirname(__file__))
+HERE = Path(__file__).parent.resolve()
 
+# The name of the project
+name = "ibis-vega-transform"
 
 def get_version(module="ibis_vega_transform"):
     """Get version."""
@@ -35,7 +44,54 @@ def read(path, encoding="utf-8"):
     return content
 
 
-setuptools.setup(
+# Get our version
+with (HERE / "package.json").open() as f:
+    version = str(parse(json.load(f)["version"]))
+
+long_description = (HERE / "README.md").read_text(errors="ignore")
+
+# Representative files that should exist after a successful build
+lab_path = HERE / name / "labextension"
+
+jstargets = [
+    str(lab_path / "package.json"),
+]
+
+package_data_spec = {name: ["*"]}
+
+labext_name = "ibis-vega-transform"
+
+data_files_spec = [
+    ("share/jupyter/labextensions/%s" % labext_name, str(lab_path), "**"),
+    ("share/jupyter/labextensions/%s" % labext_name, str(HERE), "install.json"),
+    (
+        "etc/jupyter/jupyter_server_config.d",
+        "jupyter-config/jupyter_server_config.d",
+        "jupyterlab_git.json",
+    ),
+    (
+        "etc/jupyter/jupyter_notebook_config.d",
+        "jupyter-config/jupyter_notebook_config.d",
+        "jupyterlab_git.json",
+    ),
+]
+
+cmdclass = create_cmdclass(
+    "jsdeps", package_data_spec=package_data_spec, data_files_spec=data_files_spec
+)
+
+js_command = combine_commands(
+    install_npm(HERE, build_cmd="build:prod", npm=["jlpm"]),
+    ensure_targets(jstargets),
+)
+
+is_repo = (HERE / ".git").exists()
+if is_repo:
+    cmdclass["jsdeps"] = js_command
+else:
+    cmdclass["jsdeps"] = skip_if_exists(jstargets, js_command)
+
+setup_args = dict(
     name="ibis-vega-transform",
     version=get_version(),
     url="https://github.com/Quansight/ibis-vega-transform",
@@ -43,7 +99,7 @@ setuptools.setup(
     author_email="ian.rose@quansight.com",
     license="Apache-2.0 license",
     description="Evaluate Vega transforms using Ibis expressions",
-    long_description=read("README.md"),
+    long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
     install_requires=[
@@ -53,20 +109,25 @@ setuptools.setup(
         "jaeger-client",
         "jupyter_jaeger>=1.0.3",
         "jupyterlab==3.*",
+        "jupyter_packaging~=0.7.12",
         "mypy_extensions",
-        "pymapd",
+        "pymapd==0.24.*",  # NOTE: remove this pinning when ibis v2 is ready
         "sqlalchemy<1.4",
         "tornado",
         "typing_extensions",
         "vega_datasets",
     ],
     extras_require={"dev": ["black"]},
-    python_requires=">=3.7",
+    python_requires=">=3.7,<3.8",
     include_package_data=True,
     classifiers=[
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
     ],
+    cmdclass=cmdclass,
 )
+
+
+if __name__ == '__main__':
+    setuptools.setup(**setup_args)
